da008c57618b6c64e4c0d61ed1bfe657340687f5
immikela <miketestla@gmail.com>
2015-08-05 01:22:27 +0800

    Add mouse click movement
    Add mouse click / touch event for ship movement

--- Assets/ShipControl.cs
+++ Assets/ShipControl.cs
@@ -20,5 +20,27 @@ public class ShipControl : MonoBehaviour {
 			Vector3 pos = gameObject.transform.position + new Vector3(0, 0.6f, 0);
 			Instantiate(Bullet, pos, gameObject.transform.rotation);
 		}
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch(0).phase == TouchPhase.Moved) {
+				//Vector3 pos = new Vector3 (Input.GetTouch(0).position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+				//gameObject.transform.position = pos;
+				checkTouch(Input.GetTouch(0).position);
+			}
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			//Vector3 pos = new Vector3 (Input.mousePosition.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			//gameObject.transform.position = pos;
+			checkTouch(Input.mousePosition);
+		}
+	}
+
+	void checkTouch(Vector3 pos) {
+		Vector3 wp = Camera.main.ScreenToWorldPoint (pos);
+		Vector2 touch = new Vector2 (wp.x, wp.y);
+		Collider2D hit = Physics2D.OverlapPoint (touch);
+		Debug.Log (touch.x + " " + touch.y);
+
+		Vector3 newpos = new Vector3 (touch.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		gameObject.transform.position = newpos;
 	}
 }
